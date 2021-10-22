26f5a6fce7d58e0aa0ae912559be14e55a8184d3
Fernando Montoya <montogeek@gmail.com>
2021-10-22 19:04:43 +0200

    Use Octokit

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const artifact = require('@actions/artifact');
-const artifactClient = artifact.create();
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    // const octokit = github.getOctokit(GITHUB_TOKEN);
+    const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const context = github.context;
 
-    // octokit.rest.actions.listWorkflowRunArtifacts({
-    //   owner: context.repository.owner,
-    //   repo: context.repo.repo,
-    // });
+    console.log(context.repo);
+    console.log(context.repository);
+
+    const artifacts = await octokit.rest.actions.listWorkflowRunArtifacts({
+      owner: context.repository.owner,
+      repo: context.repo.repo,
+      run_id: context.runId,
+    });
 
     console.log(context);
     console.log(JSON.stringify(context, null, 2));
+    console.log(artifacts);
 
     const prefix = core.getInput('prefix');
     const suffix = core.getInput('suffix');
