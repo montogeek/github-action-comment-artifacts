b5b21b84a7c12269a1ebae7b5ed1c54ca255e9cd
Fernando Montoya <montogeek@gmail.com>
2021-10-22 18:45:13 +0200

    First commit

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
+const github = require('@actions/github');
+const artifact = require('@actions/artifact');
+const artifactClient = artifact.create();
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    // const octokit = github.getOctokit(GITHUB_TOKEN);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const context = github.context;
 
-    core.setOutput('time', new Date().toTimeString());
+    // octokit.rest.actions.listWorkflowRunArtifacts({
+    //   owner: context.repo.owner,
+    //   repo: context.repo.repo,
+    // });
+
+    console.log(context);
+    const downloadResponse = await artifactClient.downloadAllArtifacts();
+
+    // output result
+    for (let response in downloadResponse) {
+      console.log(response.artifactName);
+      console.log(response.downloadPath);
+    }
+
+    const prefix = core.getInput('prefix');
+    const suffix = core.getInput('suffix');
+    core.info(`${prefix} ${suffix}`);
+
+    const message = `
+      ${prefix}
+      Artifacts:
+      ${suffix}
+    `
+
+    core.setOutput('message', message);
   } catch (error) {
     core.setFailed(error.message);
   }
